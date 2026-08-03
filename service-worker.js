4fdd63a6c805895960cd65555ad55231b85c8e84
khaledsabry255 <khaledsabry255@gmail.com>
2026-08-03 13:29:55 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ncm-permits-v9';
+const CACHE = 'ncm-permits-v10';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
